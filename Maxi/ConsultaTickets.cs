76dd074674034371cf9c0a0721a81fc9a6886f00
MaximoGalindo <galindomaximo0@gmail.com>
2022-10-24 23:46:18 -0300

    Formulario "terminado" (por ahi le falta algo)

--- Maxi/ConsultaTickets.cs
+++ Maxi/ConsultaTickets.cs
@@ -21,15 +21,15 @@ namespace TpLab.Maxi
 
         private void ConsultaTickets_Load(object sender, EventArgs e)
         {
-            CargarDGV();          
+           
             //CargarCombo("select * from Generos",cboGeneros);
             CargarCombo("select id_sala, descripcion from salas",cboSalas);
             
 
         }
-        public void CargarDGV()
+        public void CargarDGV(string consulta)
         {
-            DataTable dt = Consultas.consultarTabla("select t.nro_ticket 'TICKET', titulo_local 'PELICULA' ,genero 'GENERO', s.descripcion 'SALA',p.descripcion 'PROMO', t.[Precio] 'PRECIO'\r\n, nro_butaca 'BUTACA', id_comprobante 'COMPROBANTE'\r\nfrom Ticket_Precio t join Funciones as f on f.id_funcion = t.id_funcion\r\njoin Peliculas as p on f.id_pelicula = p.id_pelicula\r\njoin Butacas as b on b.id_butaca = t.id_butaca\r\njoin Salas s on s.id_sala = b.id_sala\r\njoin Peliculas_Generos pg on p.id_pelicula = pg.id_pelicula\r\njoin Generos g on pg.id_genero = g.id_genero");
+            DataTable dt = Consultas.consultarTabla(consulta);
             dgvConsultaTickets.DataSource = dt;            
         }
 
@@ -46,15 +46,24 @@ namespace TpLab.Maxi
 
         private void btnConsultar_Click(object sender, EventArgs e)
         {
-            string Consulta = "select t.nro_ticket 'TICKET', titulo_local 'PELICULA' ,genero 'GENERO', s.descripcion 'SALA',p.descripcion 'PROMO', t.[Precio] 'PRECIO'\r\n, nro_butaca 'BUTACA', id_comprobante 'COMPROBANTE'\r\nfrom Ticket_Precio t join Funciones as f on f.id_funcion = t.id_funcion\r\njoin Peliculas as p on f.id_pelicula = p.id_pelicula\r\njoin Butacas as b on b.id_butaca = t.id_butaca\r\njoin Salas s on s.id_sala = b.id_sala\r\njoin Peliculas_Generos pg on p.id_pelicula = pg.id_pelicula\r\njoin Generos g on pg.id_genero = g.id_genero";
+            string Consulta = "select t.nro_ticket 'TICKET', titulo_local 'PELICULA' ,genero 'GENERO', s.descripcion 'SALA',p.descripcion 'PROMO', t.[Precio] 'PRECIO'\r\n, nro_butaca 'BUTACA', id_comprobante 'COMPROBANTE'\r\nfrom Ticket_Precio t join Funciones as f on f.id_funcion = t.id_funcion\r\njoin Peliculas as p on f.id_pelicula = p.id_pelicula\r\njoin Butacas as b on b.id_butaca = t.id_butaca\r\njoin Salas s on s.id_sala = b.id_sala\r\njoin Peliculas_Generos pg on p.id_pelicula = pg.id_pelicula\r\njoin Generos g on pg.id_genero = g.id_genero ";
             DateTime desde = dtpDesde.Value;
             DateTime hasta = dtpHasta.Value;
             string Sala = cboSalas.Text;
             string Genero = cboGeneros.Text;
-            if (cboGeneros.Text != "Todos")
-            {
-                Consulta += "WHERE fecha between '" + desde.ToString("dd/MM/yyyy") + "' and '" + hasta.ToString("dd/MM/yyyy") + "'";
+
+            Consulta += "WHERE fecha between '" + desde.ToString("yyyy/MM/dd") + "' and '" + hasta.ToString("yyyy/MM/dd") + "'";
+            if (cboGeneros.Text != "Todos") 
+            { 
+                Consulta += " and genero like '" + Genero + "' ";
             }
+            if(cboSalas.Text != "Todos")
+            {
+               Consulta += " and s.descripcion like '" + Sala + "'";
+            }
+            
+                
+            CargarDGV(Consulta);
 
 
         }
